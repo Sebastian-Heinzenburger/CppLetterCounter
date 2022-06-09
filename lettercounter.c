a747d8fa23c46a4f0034d3f832f8b8ad1a913f9e
Sebastian <67160355+Sebastian-Heinzenburger@users.noreply.github.com>
2022-06-09 19:11:24 +0200

    Modify Output format

--- lettercounter.c
+++ lettercounter.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+
+// range of ASCII characters our program recognizes
 #define CHAR_START ' '
 #define CHAR_END '~'
 
@@ -6,18 +8,20 @@ unsigned int chars[CHAR_END - CHAR_START];
 char *input;
 
 int main(int argc, char **argv) {
-
+  // the program can read data as commandline arguments as well as from stdin
   if (argc > 1)
     input = argv[1];
   else
     scanf("%ms", &input);
 
+  // increments the value in chars[] at the character index
   for (char *c = input; *c != '\0' && *c != '\n'; c++) {
     chars[*c - CHAR_START]++;
   }
 
+  // prints the count of each character that was encountered
   for (char c = CHAR_START; c < CHAR_END; c++) {
     if (chars[c - CHAR_START] != 0)
-      printf("%c: %d\n", c, chars[c - CHAR_START]);
+      printf("%c [0x%X]: %d\n", c, c, chars[c - CHAR_START]);
   }
 }
